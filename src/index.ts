38e950e0cb61081a7b9807cc352b153f28d037f5
Malyshev <getdimamal@gmail.com>
2024-03-30 14:24:53 +0300

    Initial Project

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import './styles/main.scss';
+
+document.addEventListener('DOMContentLoaded', () => {
+    const root = document.getElementById('root');
+
+    if (root) {
+        root.innerHTML = `<h1>Hello Calculator App!</h1>`;
+    }
+});
