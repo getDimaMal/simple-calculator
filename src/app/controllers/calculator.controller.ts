f1f110f99ee931595afa5423de31e8ab9898d56b
Malyshev <getDimaMal@gmail.com>
2024-04-01 14:44:05 +0300

    Add CalculatorController

---
+++ src/app/controllers/calculator.controller.ts
@@ -0,0 +1,61 @@
+import { CalculatorView } from '../views/calculator.view';
+
+const keys = [
+    { label: 'C', type: 'function' },
+    { label: '+/-', type: 'function' },
+    { label: '%', type: 'function' },
+    { label: '/', type: 'operator' },
+
+    { label: '1', type: 'value' },
+    { label: '2', type: 'value' },
+    { label: '3', type: 'value' },
+    { label: 'x', type: 'operator' },
+
+    { label: '4', type: 'value' },
+    { label: '5', type: 'value' },
+    { label: '6', type: 'value' },
+    { label: '-', type: 'operator' },
+
+    { label: '7', type: 'value' },
+    { label: '8', type: 'value' },
+    { label: '9', type: 'value' },
+    { label: '+', type: 'operator' },
+
+    { label: '⌫', type: 'function' },
+    { label: '0', type: 'value' },
+    { label: '.', type: 'value' },
+    { label: '=', type: 'operator' },
+] as ConstructorParameters<typeof CalculatorView>[0]['keys'];
+
+export class CalculatorController {
+    private view: CalculatorView;
+
+    constructor() {
+        this.view = new CalculatorView({
+            keys,
+            displayValue: '0',
+            handlers: {
+                value: this.handleValue.bind(this),
+                operator: this.handleOperator.bind(this),
+                function: this.handleFunction.bind(this),
+            },
+        });
+    }
+
+    private handleValue(value: string) {
+        this.view.displayUpdate(value);
+        console.log('value', value);
+    }
+
+    private handleOperator(value: string) {
+        console.log('operator', value);
+    }
+
+    private handleFunction(value: string) {
+        console.log('function', value);
+    }
+
+    render() {
+        return this.view.render();
+    }
+}
